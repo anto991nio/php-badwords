e09fbe61a6a50fd355a325a1dc30d20c4ab3600d
anto991nio <antonio.91.cardone@gmail.com>
2021-05-28 15:41:53 +0200

    Termina eserciizo

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ $paragrafo ="Ciao Ciao Ciao Ciao Ciao Ciao Ciao";
 $badWord = $_GET["badword"];
 $newWord = str_replace($badWord, "***",$badWord);
 
+echo strlen($paragrafo);
+
+echo "<br>";
+
 echo $paragrafo . " " . $newWord;
 
 ?>
